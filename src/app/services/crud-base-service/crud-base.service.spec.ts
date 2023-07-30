99b54dc2379b1fdf8201b1f40ea6fb2ad410f229
BuildTools <unconfigured@null.spigotmc.org>
2023-07-29 23:08:18 -0300

    fix: testes unitarios

--- src/app/services/crud-base-service/crud-base.service.spec.ts
+++ src/app/services/crud-base-service/crud-base.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CrudBaseService } from './crud-base.service';
+import { PessoaDTO } from 'src/app/models/pessoaDTO';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CrudBaseService', () => {
-  let service: CrudBaseService;
+  let service: CrudBaseService<PessoaDTO>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule],
+    });
     service = TestBed.inject(CrudBaseService);
   });
 
